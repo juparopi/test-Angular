b05a15e16331073f68d5763b0a8855426db52e78
Juan Romero <juan.romero@bizagi.com>
2023-10-29 18:36:49 -0500

    Leccion 1 Semana 2 1.0

--- hola-mundo/src/app/models/destino-viaje.model.ts
+++ hola-mundo/src/app/models/destino-viaje.model.ts
@@ -1,7 +1,9 @@
 export class DestinoViaje{
     private selected: boolean;
+    servicios: String[];
     constructor(public nombre:string, public imageUrl:string) {
         this.selected = false;
+        this.servicios = ['piscina', 'desayunos'];
     }
     
     isSelected(): boolean{
